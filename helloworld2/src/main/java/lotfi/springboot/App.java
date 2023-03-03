aed8ada0495a02eb8aeae7ca14b3b6a8cf2dc8a8
lotfi wahbi <lwahbi@gmail.com>
2023-03-03 20:09:16 +0100

    First commit

---
+++ helloworld2/src/main/java/lotfi/springboot/App.java
@@ -0,0 +1,16 @@
+package lotfi.springboot;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(StringUtils.capitalize("hello World!") );
+    }
+}
